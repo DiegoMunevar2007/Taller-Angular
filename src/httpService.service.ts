66d5407afae68b84a8db023a24c90fb753e72b24
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2025-04-17 18:31:18 -0500

    Inicio

---
+++ src/httpService.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+constructor(private http: HttpClient) { }
+
+public getSeries(){
+  return this.http.get("https://gist.githubusercontent.com/josejbocanegra/8490b48961a69dcd2bfd8a360256d0db/raw/34ff30dbc32392a69eb0e08453a3fc975a3890f0/series.json")
+}
+}
